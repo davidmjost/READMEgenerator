3678c3522355e2fa880d5fa2753c73ab04a935a6
David Jost <82765414+davidmjost@users.noreply.github.com>
2021-08-24 18:52:01 -0400

    create generateMD.js
    add initial generateMD.js with badge script

---
+++ utils/generateMD.js
@@ -0,0 +1,14 @@
+function badge(license) {
+    if (license === "MIT") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    }
+    else if (license === "SIL") {
+        return "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL%201.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)"
+    }
+    else if (license === "IBM") {
+        return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+    }
+    else {
+        return ""
+    };
+};
